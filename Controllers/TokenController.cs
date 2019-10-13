f75c0946b333dc04fc135022ed5c3250d29fc219
Will <doggy.huang@gmail.com>
2019-10-14 00:29:47 +0800

    完成 JWT Token-based 身分驗證與授權機制

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using JwtAuthDemo.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly JwtHelpers jwt;
+
+        public TokenController(JwtHelpers jwt)
+        {
+            this.jwt = jwt;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("~/signin")]
+        public ActionResult<string> SignIn(LoginViewModel login)
+        {
+            if (ValidateUser(login))
+            {
+                return jwt.GenerateToken(login.Username);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private bool ValidateUser(LoginViewModel login)
+        {
+            return true; // TODO
+        }
+
+        [HttpGet("~/claims")]
+        public IActionResult GetClaims()
+        {
+            return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
+        }
+
+        [HttpGet("~/username")]
+        public IActionResult GetUserName()
+        {
+            return Ok(User.Identity.Name);
+        }
+
+        [HttpGet("~/jwtid")]
+        public IActionResult GetUniqueId()
+        {
+            var jti = User.Claims.FirstOrDefault(p => p.Type == "jti");
+            return Ok(jti.Value);
+        }
+    }
+
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using JwtAuthDemo.Helpers;
 
 namespace JwtAuthDemo
 {
@@ -25,6 +29,28 @@ namespace JwtAuthDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<JwtHelpers>();
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.IncludeErrorDetails = true; // WWW-Authenticate 會顯示詳細錯誤原因
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+                        RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+                        ValidateIssuer = true,
+                        ValidIssuer = Configuration.GetValue<string>("JwtSettings:Issuer"),
+                        ValidateAudience = false,
+                        //ValidAudience = Configuration.GetValue<string>("JwtSettings:Issuer"),
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = false, // 如果 JWT 包含 key 才需要驗證，一般都只有簽章而已
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("JwtSettings:SignKey")))
+                    };
+                });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,6 +66,8 @@ namespace JwtAuthDemo
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
             app.UseMvc();
