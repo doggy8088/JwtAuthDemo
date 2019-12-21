97c6ccbdb7858f60186e3137001dc6c63881e29a
Will <doggy.huang@gmail.com>
2019-12-21 18:26:37 +0800

    在 ASP․NET Core 3.1 完整設定 NSwag 與 OpenAPI v3 文件範例程式碼

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -8,9 +8,14 @@ using JwtAuthDemo.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NSwag.Annotations;
 
 namespace JwtAuthDemo.Controllers
 {
+    /// <summary>
+    /// 主要負責 JWT Token 相關操作
+    /// </summary>
     [Authorize]
     [ApiController]
     public class TokenController : ControllerBase
@@ -22,8 +27,22 @@ namespace JwtAuthDemo.Controllers
             this.jwt = jwt;
         }
 
+        /// <summary>
+        /// 登入並取得 JWT Token
+        /// </summary>
+        /// <param name="login">LoginViewModel</param>
+        /// <returns>回傳一個 JWT Token 字串</returns>
+        /// <example> // 這個沒效！回傳純字串是沒效的，要自訂 ViewModel 才能設定範例到特定屬性去
+        /// "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsImtpZCI6bnVsbCwidHlwIjoiSldUIn0.eyJleHAiOiIxNTc2NTA5NzA0IiwiaXNzIjoiSnd0QXV0aERlbW8iLCJpYXQiOiIxNTc2NTA3OTA0IiwibmJmIjoiMTU3NjUwNzkwNCJ9.XmagvhyW_6SUFJfiOahkOBuVlLjyogEzMba3-WlbNmI"
+        /// </example>
+        /// <response code="200">成功產生 JWT Token</response>
+        /// <response code="400">登入帳號或密碼錯誤</response>
         [AllowAnonymous]
         [HttpPost("~/signin")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(ProblemDetails), 400)]
         public ActionResult<string> SignIn(LoginViewModel login)
         {
             if (ValidateUser(login))
@@ -41,29 +60,81 @@ namespace JwtAuthDemo.Controllers
             return true; // TODO
         }
 
+        /// <summary>
+        /// 取得所有 Claims 清單 (只有 Role="admin" 可呼叫)
+        /// </summary>
+        /// <returns>IEnumerable&lt;Claim&gt;</returns>
+        /// <response code="200" nullable="true">取得所有 Claims 清單</response>
+        /// <response code="401">需提供 Bearer Token 才能呼叫</response>
+        [Authorize(Roles = "admin")]
         [HttpGet("~/claims")]
-        public IActionResult GetClaims()
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        public ActionResult<IEnumerable<Claim>> GetClaims()
         {
             return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
         }
 
+        /// <summary>
+        /// 取得使用者名稱 (未登入也可以執行，但會回傳 HTTP 204 且沒有內容)
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">使用者名稱</response>
+        /// <response code="204" nullable="true">查無使用者名稱</response>
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(string), 200)]
+        [AllowAnonymous]
         [HttpGet("~/username")]
         public IActionResult GetUserName()
         {
             return Ok(User.Identity.Name);
         }
 
+        /// <summary>
+        /// 取得 JWT ID (需登入才能取得資訊)
+        /// </summary>
+        /// <returns>JWT ID</returns>
+        /// <response code="200">JWT ID</response>
+        /// <response code="401">需提供 Bearer Token 才能呼叫</response>
+        /// <response code="404">查無 JWT ID</response>
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(void), 404)]
+        [ProducesResponseType(401)]
         [HttpGet("~/jwtid")]
         public IActionResult GetUniqueId()
         {
             var jti = User.Claims.FirstOrDefault(p => p.Type == "jti");
-            return Ok(jti.Value);
+            if (jti == null)
+            {
+                // 預設 HTTP 404 都會以 ProblemDetails 型別回應，不回傳內容要給 null
+                return StatusCode(404, null);
+            }
+            else
+            {
+                return Ok(jti.Value);
+            }
         }
     }
 
+    /// <summary>
+    /// 登入模型
+    /// </summary>
     public class LoginViewModel
     {
+        /// <summary>
+        /// 使用者名稱
+        /// </summary>
+        /// <example>"will"</example>
         public string Username { get; set; }
+
+        /// <summary>
+        /// 使用者密碼
+        /// </summary>
+        /// <example>"YourPassword"</example>
         public string Password { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -15,6 +16,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using JwtAuthDemo.Helpers;
 using Microsoft.Extensions.Hosting;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 
 namespace JwtAuthDemo
 {
@@ -52,7 +55,48 @@ namespace JwtAuthDemo
                     };
                 });
 
-            services.AddControllers();
+            // add OpenAPI v3 document
+            services.AddOpenApiDocument(config =>
+            {
+                // 設定文件名稱 (重要) (預設值: v1)
+                config.DocumentName = "v2";
+
+                // 設定文件或 API 版本資訊
+                config.Version = "0.0.1";
+
+                // 設定文件標題 (當顯示 Swagger/ReDoc UI 的時候會顯示在畫面上)
+                config.Title = "JwtAuthDemo";
+
+                // 設定文件簡要說明
+                config.Description = "This is a JWT authentication/authorization sample app";
+
+                // 是否要顯示 API 呼叫範例
+                // config.GenerateExamples = true;
+
+                var apiScheme = new OpenApiSecurityScheme()
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "Authorization",
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Description = "Copy this into the value field: Bearer {token}"
+                };
+
+                config.AddSecurity("JWT Token", Enumerable.Empty<string>(), apiScheme);
+                // config.AddSecurity("API Token", Enumerable.Empty<string>(), apiScheme);
+
+                config.OperationProcessors.Add(
+                    new AspNetCoreOperationSecurityScopeProcessor("JWT Token"));
+
+                config.DefaultResponseReferenceTypeNullHandling = NJsonSchema.Generation.ReferenceTypeNullHandling.Null;
+            });
+
+            // add Swagger v2 document
+            // services.AddSwaggerDocument();
+
+            services.AddControllers(options =>
+            {
+                options.OutputFormatters.RemoveType<StringOutputFormatter>();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,8 +119,76 @@ namespace JwtAuthDemo
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => {
-               endpoints.MapControllers();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // serve OpenAPI/Swagger documents
+            // services.AddOpenApiDocument();
+            // app.UseOpenApi();
+
+            app.UseOpenApi(config =>
+            {
+                // 這裡的 Path 用來設定 OpenAPI 文件的路由 (網址路徑) (一定要以 / 斜線開頭)
+                config.Path = "/swagger/v2/swagger.json";
+
+                // 這裡的 DocumentName 必須跟 services.AddOpenApiDocument() 的時候設定的 DocumentName 一致！
+                config.DocumentName = "v2";
+
+                config.PostProcess = (document, http) =>
+                {
+                    if (env.IsDevelopment())
+                    {
+                        document.Info.Title += " (開發環境)";
+                        document.Info.Version += "-dev";
+                        document.Info.Description = "當 API 有問題時，請聯繫 Will 保哥的技術交流中心 粉絲團，我們有專業顧問可以協助解決困難！";
+                        document.Info.Contact = new NSwag.OpenApiContact
+                        {
+                            Name = "Will Huang",
+                            Email = "doggy.huang@gmail.com",
+                            Url = "https://twitter.com/Will_Huang"
+                        };
+                    }
+                    else
+                    {
+                        document.Info.TermsOfService = "https://go.microsoft.com/fwlink/?LinkID=206977";
+
+                        document.Info.Contact = new NSwag.OpenApiContact
+                        {
+                            Name = "Will Huang",
+                            Email = "doggy.huang@gmail.com",
+                            Url = "https://twitter.com/Will_Huang"
+                        };
+                    }
+
+                    document.Info.License = new NSwag.OpenApiLicense
+                    {
+                        Name = "The MIT License",
+                        Url = "https://opensource.org/licenses/MIT"
+                    };
+                };
+            });
+
+            app.UseSwaggerUi3(config =>
+            {
+                // 這裡的 Path 用來設定 Swagger UI 的路由 (網址路徑) (一定要以 / 斜線開頭)
+                config.Path = "/swagger";
+
+                // 這裡的 DocumentPath 用來設定 OpenAPI 文件的網址路徑 (一定要以 / 斜線開頭)
+                config.DocumentPath = "/swagger/v2/swagger.json";
+
+                // 這裡的 DocExpansion 用來設定 Swagger UI 是否要展開文件 (可設定為 none, list, full，預設: none)
+                config.DocExpansion = "list";
+            });
+
+            app.UseReDoc(config =>
+            {
+                // 這裡的 Path 用來設定 ReDoc UI 的路由 (網址路徑) (一定要以 / 斜線開頭)
+                config.Path = "/redoc";
+
+                // 這裡的 DocumentPath 用來設定 OpenAPI 文件的網址路徑 (一定要以 / 斜線開頭)
+                config.DocumentPath = "/swagger/v2/swagger.json";
             });
         }
     }
